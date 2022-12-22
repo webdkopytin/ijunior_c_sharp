ea524a2c9eb22d21ed613083abd57ae4e0a04495
Dmitrii Kopytin <web@dkopytin.ru>
2022-12-22 17:54:51 +0300

    basic logic

--- 003_arrays/004_Dynamic array/ArrayApp4/Program.cs
+++ 003_arrays/004_Dynamic array/ArrayApp4/Program.cs
@@ -6,14 +6,42 @@ namespace ArrayApp4
     {
         static void Main(string[] args)
         {
-            //Пользователь вводит числа, и программа их запоминает.
-            //Как только пользователь введёт команду sum, программа выведет сумму всех веденных чисел.
-            //Выход из программы должен происходить только в том случае, если пользователь введет команду exit.
-            //Если введено не sum и не exit, значит это число и его надо добавить в массив.
-            //Программа должна работать на основе расширения массива.
-            //Внимание, нельзя использовать List<T> и Array.Resize
+            bool isOpen = true;
 
+            int[] saveNumbers = new int[0];
+            int[] tempNumbers;
 
+            while (isOpen)
+            {
+
+                Console.Clear();
+                Console.SetCursorPosition(0, 20);
+                Console.Write("Запомненные числа: ");
+
+                foreach (int number in saveNumbers)
+                {
+                    Console.Write($"{number} ");
+                }
+
+                tempNumbers = new int[saveNumbers.Length + 1];
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Доступны команды:\nsum - выводит сумму всех введенных чисел\nexit - выход из программы");
+                Console.Write("\nВведите число, а программа его запомнит: ");
+                int inputUserNumber = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < saveNumbers.Length; i++)
+                {
+                    tempNumbers[i] = saveNumbers[i];
+                }
+
+                tempNumbers[tempNumbers.Length - 1] = inputUserNumber;
+                saveNumbers = tempNumbers;
+
+                //TODO: Доделать выполнение sum, exit
+            }
+
+            Console.ReadKey();
         }
     }
 }
