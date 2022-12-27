ce361d3548d45c315c0f34bb4cb0b0b00d8767e9
Dmitrii Kopytin <web@dkopytin.ru>
2022-12-27 15:15:57 +0300

    performing bubble sorting

--- 003_arrays/006_SortingNumbers/006_SortingNumbers/Program.cs
+++ 003_arrays/006_SortingNumbers/006_SortingNumbers/Program.cs
@@ -6,9 +6,39 @@ namespace _006_SortingNumbers
     {
         static void Main(string[] args)
         {
-            // Дан массив чисел(минимум 10 чисел).Надо вывести в консоль числа отсортированы, от меньшего до большего.
-            // Нельзя использовать Array.Sort.Можно найти подходящий алгоритм сортировки и использовать его для задачи.
+            int lengthArray = 10;
+            int firstTempNumber;
+            int secondTempNumber;
+            int firstRandomNumber = 1;
+            int lastRandomNumber = 10;
 
+            int[] array = new int[lengthArray];
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(firstRandomNumber, lastRandomNumber);
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                firstTempNumber = array[i - 1];
+
+                if (firstTempNumber > array[i])
+                {
+                    secondTempNumber = firstTempNumber;
+                    firstTempNumber = array[i];
+                    array[i] = secondTempNumber;
+                }
+            }
+
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
