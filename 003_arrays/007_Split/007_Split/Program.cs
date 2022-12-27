3db4daf6812e16edc30a08f264cf7126728bb4dd
Dmitrii Kopytin <web@dkopytin.ru>
2022-12-27 15:24:20 +0300

    init task

---
+++ 003_arrays/007_Split/007_Split/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _007_Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Дана строка с текстом, используя метод строки String.Split() получить массив слов,
+            // которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.
+
+        }
+    }
+}
