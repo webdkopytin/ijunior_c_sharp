044ec9fbe2c57c684a13a7e39de634e4ec6d28bd
Dmitrii Kopytin <web@dkopytin.ru>
2022-12-29 11:18:33 +0300

    fix: shift array at the user request

--- 003_arrays/008_ShiftingArrayValues/008_ShiftingArrayValues/Program.cs
+++ 003_arrays/008_ShiftingArrayValues/008_ShiftingArrayValues/Program.cs
@@ -7,9 +7,12 @@ namespace _008_ShiftingArrayValues
         static void Main(string[] args)
         {
             int tempNumber;
-            int countShift = 2;
+            int countShift;
 
             int[] array = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            Console.Write("Введите число, на которое сдвинуть массив: ");
+            countShift = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 0; i < array.Length; i++)
             {
