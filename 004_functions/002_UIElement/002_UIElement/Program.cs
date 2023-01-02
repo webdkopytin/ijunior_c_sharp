96aa99c6761d869ceeeac4ac27fdd0d3ca5ee53a
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-02 10:55:53 +0300

    add: task UIElement

---
+++ 004_functions/002_UIElement/002_UIElement/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _002_UIElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Разработайте функцию, которая рисует некий бар(Healthbar, Manabar) в определённой позиции.
+            // Она также принимает некий закрашенный процент.
+            // При 40 % бар выглядит так:
+            // [####______]
+        }
+    }
+}
