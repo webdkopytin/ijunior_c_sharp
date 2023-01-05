acbd6b99588ee8222b8eeb52c88d9ccab03c42d1
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-05 18:34:25 +0300

    add: logic in the program

--- 004_functions/002_UIElement/002_UIElement/Program.cs
+++ 004_functions/002_UIElement/002_UIElement/Program.cs
@@ -6,10 +6,50 @@ namespace _002_UIElement
     {
         static void Main(string[] args)
         {
-            // Разработайте функцию, которая рисует некий бар(Healthbar, Manabar) в определённой позиции.
-            // Она также принимает некий закрашенный процент.
-            // При 40 % бар выглядит так:
-            // [####______]
+            int health = 3, maxHealth = 10;
+            int mana = 3, maxMana = 10;
+
+            while (true)
+            {
+                DrawBar(health, maxHealth, ConsoleColor.Red, 0, '_');
+                DrawBar(mana, maxMana, ConsoleColor.Blue, 1, '_');
+
+                Console.SetCursorPosition(0, 5);
+
+                Console.Write("Введите на сколько изменятся жизни: ");
+                health += Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите на сколько изменится мана: ");
+                mana += Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+            }
+        }
+
+        static void DrawBar(int value, int maxValue, ConsoleColor color, int position, char symbol = ' ')
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+            string bar = "";
+
+            for (int i = 0; i < value; i++)
+            {
+                bar += symbol;
+            }
+
+            Console.SetCursorPosition(0, position);
+            Console.Write('[');
+            Console.BackgroundColor = color;
+            Console.Write(bar);
+            Console.BackgroundColor = defaultColor;
+
+            bar = "";
+
+            for (int i = value; i < maxValue; i++)
+            {
+                bar += symbol;
+            }
+
+            Console.Write(bar + ']');
         }
     }
 }
