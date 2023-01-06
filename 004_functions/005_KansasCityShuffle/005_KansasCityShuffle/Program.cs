d0408b51659bc469e9739a4480b089ec06f3af57
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-06 21:43:41 +0300

    add: task "Kansas City Shuffle"

---
+++ 004_functions/005_KansasCityShuffle/005_KansasCityShuffle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _005_KansasCityShuffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Реализуйте функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
+        }
+    }
+}
