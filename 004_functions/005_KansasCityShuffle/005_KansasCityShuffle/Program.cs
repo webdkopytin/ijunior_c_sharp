2c27e527480f28211b2013fa4a5cd5ef68041901
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-10 16:06:32 +0300

    add: logic

--- 004_functions/005_KansasCityShuffle/005_KansasCityShuffle/Program.cs
+++ 004_functions/005_KansasCityShuffle/005_KansasCityShuffle/Program.cs
@@ -6,7 +6,32 @@ namespace _005_KansasCityShuffle
     {
         static void Main(string[] args)
         {
-            // Реализуйте функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
+            string name = "АБВГД";
+
+            foreach (char resultChar in Shuffle(name))
+            {
+                Console.Write(resultChar);
+            }
+
+            Console.ReadKey();
+        }
+
+        static char[] Shuffle(string name)
+        {
+            int newRandIndex;
+
+            Random random = new Random();
+
+            char[] tempChars = name.ToCharArray();
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                newRandIndex = random.Next(i);
+
+                (tempChars[i], tempChars[newRandIndex]) = (tempChars[newRandIndex], tempChars[i]);
+            }
+
+            return tempChars;
         }
     }
 }
