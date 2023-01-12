f0a59fdb5337615b69f29c91299cad73e7dce8bb
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-12 16:17:25 +0300

    add: task "explanatory dictionary"

---
+++ 005_сollections/001_ExplanatoryDictionary/001_ExplanatoryDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _001_ExplanatoryDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создать программу, которая принимает от пользователя слово и выводит его значение.
+            // Если такого слова нет, то следует вывести соответствующее сообщение.
+        }
+    }
+}
