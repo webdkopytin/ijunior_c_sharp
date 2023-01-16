cad1fc6941727cec2aaeff03a7eed06f7162e46f
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-16 12:24:27 +0300

    fix: name variables

--- 004_functions/003_ReadIint/003_ReadIint/Program.cs
+++ 004_functions/003_ReadIint/003_ReadIint/Program.cs
@@ -6,25 +6,25 @@ namespace _003_ReadIint
     {
         static void Main(string[] args)
         {
-            int resultParse = ReceiveNumber();
+            int resultParse = GetNumber();
 
             Console.WriteLine("Выполнено успешно! Сконвертированное число: " + resultParse);
             Console.ReadKey();
         }
 
-        static int ReceiveNumber()
+        static int GetNumber()
         {
             int result = 0;
 
-            bool isOpen = true;
+            bool isWorking = true;
 
-            while (isOpen)
+            while (isWorking)
             {
                 Console.Write("Попытка конвертации в тип int (с помощью int.TryParse): ");
                 string stringToNumber = Console.ReadLine();
 
                 if (int.TryParse(stringToNumber, out result))
-                    isOpen = false;
+                    isWorking = false;
                 else
                     Console.WriteLine("Неуспешно, повторная попытка...");
             }
