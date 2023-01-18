fae6ae2063315aca4ba905c5255d9c68bb038f27
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-18 15:17:15 +0300

    add: queue

--- 005_сollections/002_QueueStore/002_QueueStore/Program.cs
+++ 005_сollections/002_QueueStore/002_QueueStore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace _002_QueueStore
@@ -12,6 +13,23 @@ namespace _002_QueueStore
             // После каждого обслуженного клиента деньги нужно добавлять на наш счёт и выводить его в консоль.
             // После обслуживания каждого клиента программа ожидает нажатия любой клавиши, после чего затирает консоль и по новой выводит всю информацию,
             // только уже со следующим клиентом
+            int deposit = 0;
+
+            Dictionary<int, int[]> queueBuyers = new Dictionary<int, int[]>()
+            {
+                [1] = new int[] { 1433, 300, 434, 595, 6333 },
+                [2] = new int[] { 100, 344, 4234 },
+                [3] = new int[] { 234, 3454, 1238 }
+            };
+
+            Queue<int> purchaseSize = new Queue<int>();
+
+            for (int i = 1; i <= queueBuyers.Count; i++)
+            {
+                Console.WriteLine(queueBuyers);
+            }
+
+            Console.ReadKey();
         }
     }
 }
