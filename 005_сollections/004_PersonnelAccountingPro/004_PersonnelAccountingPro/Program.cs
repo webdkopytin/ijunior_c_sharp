d0135981bc54a4c3e6155ee309daaa39f97478d7
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-19 17:05:04 +0300

    add: task "personnel accounting pro"

---
+++ 005_сollections/004_PersonnelAccountingPro/004_PersonnelAccountingPro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_PersonnelAccountingPro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // В функциях вы выполняли задание "Кадровый учёт"
+            // Используя одну из изученных коллекций, вы смогли бы сильно себе упростить код выполненной программы, ведь у нас данные, это ФИО и позиция.
+            // Поиск в данном задании не нужен.
+
+            // 1) добавить досье
+            // 2) вывести все досье(в одну строку через “-” фио и должность)
+            // 3) удалить досье
+            // 4) выход
+        }
+    }
+}
