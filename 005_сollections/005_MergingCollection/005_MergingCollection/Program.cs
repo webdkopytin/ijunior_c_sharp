90a82d5f0f7a4294ba07a5b834ed7c98d0c10b1c
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-20 10:05:37 +0300

    add: task "merging into one collection"

---
+++ 005_сollections/005_MergingCollection/005_MergingCollection/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_MergingCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Есть два массива строк. Надо их объединить в одну коллекцию, исключив повторения, не используя Linq.
+            // Пример: { "1", "2", "1"} + { "3", "2"} => { "1", "2", "3"}
+        }
+    }
+}
