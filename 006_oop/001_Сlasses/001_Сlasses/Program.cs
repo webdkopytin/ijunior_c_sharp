bda41053abb36d6c018032feeb32a560386049a6
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-25 08:38:32 +0300

    add: init task

---
+++ 006_oop/001_Сlasses/001_Сlasses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Сlasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создать класс игрока, с полями, содержащими информацию об игроке и методом, который выводит информацию на экран.
+            // В классе обязательно должен быть конструктор
+        }
+    }
+}
