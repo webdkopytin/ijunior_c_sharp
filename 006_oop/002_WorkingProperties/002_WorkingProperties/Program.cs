14997f59a1646a6ebbac83d7a6bc9d9645bcfde6
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-27 08:53:32 +0300

    add: init task

---
+++ 006_oop/002_WorkingProperties/002_WorkingProperties/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_WorkingProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создать класс игрока, у которого есть поля с его положением в x,y.
+            // Создать класс отрисовщик, с методом, который отрисует игрока.
+
+            // Попрактиковаться в работе со свойствами.
+        }
+    }
+}
