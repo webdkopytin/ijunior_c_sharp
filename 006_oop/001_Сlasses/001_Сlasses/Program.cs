f08afce33ceb0a13ee61dbd8f80338380f57c08e
Dmitrii Kopytin <web@dkopytin.ru>
2023-01-27 08:41:39 +0300

    fix: field access

--- 006_oop/001_Сlasses/001_Сlasses/Program.cs
+++ 006_oop/001_Сlasses/001_Сlasses/Program.cs
@@ -15,20 +15,20 @@ namespace _001_Сlasses
 
         class Player
         {
-            public string Name;
-            public int Health;
-            public int Mana;
+            private string _name;
+            private int _health;
+            private int _mana;
 
             public Player(string name, int health, int mana)
             {
-                this.Name = name;
-                this.Health = health;
-                this.Mana = mana;
+                _name = name;
+                _health = health;
+                _mana = mana;
             }
 
             public void ShowInfo()
             {
-                Console.WriteLine($"Имя игрока: {Name}\nКоличество жизней: {Health}\nКоличество маны: {Mana}");
+                Console.WriteLine($"Имя игрока: {_name}\nКоличество жизней: {_health}\nКоличество маны: {_mana}");
             }
         }
     }
